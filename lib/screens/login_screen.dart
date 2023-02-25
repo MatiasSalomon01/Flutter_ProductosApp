e1cce1484f005b4aa9e363f12b22ffb65a7d3f78
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-25 20:08:17 -0300

    Formulario de Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,20 +13,62 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 250),
+
               CardContainer(
                 child: Column(
                   children: [
                     SizedBox(height: 10,),
                     Text('Login', style: Theme.of(context).textTheme.headlineMedium),
                     SizedBox(height: 10,),
-                    Text('Formulario')
+                    _LoginForm()
                   ],
                 )
               ),
+              SizedBox(height: 50,),
+              Text('Crear una nueva cuenta', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
             ],
           ),
         ),
       )
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                hintText: 'matisalomond@gmail.com',
+                labelText: 'Correo',
+                labelStyle: TextStyle(
+                  color: Colors.deepPurple
+                ),
+                prefixIcon: Icon(Icons.alternate_email_outlined, color: Colors.deepPurple,)
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
