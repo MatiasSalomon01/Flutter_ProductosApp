02d210fa9d4d6a30c49744a8fe0520b44a2fdd90
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-25 22:07:26 -0300

    Widget - ProductCard

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -10,7 +10,59 @@ class ProductCard extends StatelessWidget {
         margin: EdgeInsets.only(top: 30, bottom: 50),
         height: 400,
         width: double.infinity,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+            _productDetails()
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(25),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black12,
+        offset: Offset(0, 6),
+        blurRadius: 10
+      )
+    ]
+  );
+}
+
+class _productDetails extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: double.infinity,
+      color: Colors.red,
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        height: 400,
+        width: double.infinity,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+        ),
       ),
     );
   }
