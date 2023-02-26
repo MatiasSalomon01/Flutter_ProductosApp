12a00545c3c189c614e5fe294fdbe56dd904520f
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-26 20:10:08 -0300

    Grabar Token en el Secure Storage

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = 'AIzaSyBq3mXkFymRl2M7896TerQQVSeUVPP99-g';
+
+  final storage = FlutterSecureStorage();
 
   Future<String?> createUser(String email, String password) async {
     final Map<String, dynamic> authData = {
@@ -26,11 +28,43 @@ class AuthService extends ChangeNotifier {
 
     if(decodedRes.containsKey('idToken')){
       //Guardar token en un lugar seguro
+      await storage.write(key: 'token', value: decodedRes['idToken']);
       // return decodedRes['idToken'];
       return null;
     }else{
       return decodedRes['error']['message'];
     }
-
   }
+
+  Future<String?> login(String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email' : email,
+      'password' : password
+    };
+
+    final url = Uri.https(_baseUrl, '/v1/accounts:signInWithPassword',{
+      'key': _firebaseToken
+    });
+
+    final res = await http.post(url, body: json.encode(authData));
+
+    final Map<String, dynamic> decodedRes = json.decode(res.body);
+
+    //print(decodedRes);
+
+    if(decodedRes.containsKey('idToken')){
+      //Guardar token en un lugar seguro
+      await storage.write(key: 'token', value: decodedRes['idToken']);
+      // return decodedRes['idToken'];
+      return null;
+    }else{
+      return decodedRes['error']['message'];
+    }
+  }
+
+  Future logout() async{
+    await storage.delete(key: 'token');
+    return;
+  }
+
 }
